70baefbee24628ffd8e805d1b92bf90dbc3b073b
脇村 隆 <wakimura@plus-movement.jp>
2018-01-03 00:05:24 +0900

    コミット漏れ（正しくはセーブ漏れ）

--- lockets.php
+++ lockets.php
@@ -4,7 +4,7 @@ Plugin Name: Lockets
 Plugin URI: http://lockets.jp/
 Description: A plug-in that gets information on spots such as shops and inns from various APIs and displays the latest information embedded in the blog.Also, This plugin will assist you such as creating affiliate links. お店や旅館などスポットに関する情報を各種APIから取得し、ブログ内に最新の情報を埋め込んで表示するプラグイン。また、アフィリエイトリンク作成支援を行います。
 Author: wackey
-Version: 0.53
+Version: 0.54
 Author URI: https://musilog.net/
 License: GPL2
 */
