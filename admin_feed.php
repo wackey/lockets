77442fb507fd7335ee7041d7ef1c3f5a9e0aaee8
脇村隆 <takashi.wakimura@gmail.com>
2019-09-16 23:59:21 +0900

    管理画面の微修正、Lockets、SmartFormat明記、設定したfeedURLのリンク設定

--- admin_feed.php
+++ admin_feed.php
@@ -25,7 +25,7 @@ $lockets_feedlogourl = get_option('lockets_feedlogourl');
 
 <div class="wrap">
     <h2>Locketsプラグイン：外部へのfeed配信β＜開発中/テスター向け＞</h2>
-    <p>外部サイトへ配信するfeedを有効にする場合はONとしてください</p>
+    <p>外部サイトへ配信するfeedを有効にする場合はONとしてください。LocketsおよびSmartFormatに準拠したfeedを出力します（β版）。</p>
     <form name="form" method="post" action="">
         <input type="hidden" name="action" value="update" />
         <?php wp_nonce_field('lockets-options'); ?>
@@ -36,7 +36,7 @@ $lockets_feedlogourl = get_option('lockets_feedlogourl');
                 <tr>
                     <th><label for="locketsfeedswitch"><?php
 _e('外部配信を有効にする', 'lockets_feedswitch'); ?></label></th>
-                    <td><? echo $lockets_feedswitch; ?>
+                    <td>
                         <?php if($lockets_feedswitch == "1"){ ?>
                         <input type="radio" name="lockets_feedswitch" value="1" checked>ON
                         <input type="radio" name="lockets_feedswitch" value="0">OFF
@@ -52,14 +52,17 @@ _e('外部配信を有効にする', 'lockets_feedswitch'); ?></label></th>
 _e('外部配信用URL設定', 'lockets_feedurl'); ?></label></th>
                     <td><input size="36" type="text" name="lockets_feedurl" id="lockets_feedurl" value="<?php
 echo attribute_escape($lockets_feedurl); ?>" /><br>
-                    <?php echo site_url()."/?feed=".attribute_escape($lockets_feedurl); ?></td>
+                        <?php if ($lockets_feedurl !== "") { ?>
+                    設定したURLは“ <a href="<?php echo site_url()."/?feed=".attribute_escape($lockets_feedurl); ?>"><?php echo site_url()."/?feed=".attribute_escape($lockets_feedurl); ?></a> ”です
+                        <?php } ?>
+                    </td>
                 </tr>
 
                 <tr>
                     <th><label for="lockets_feedlogourl"><?php
 _e('logoの画像URL', 'lockets_feedlogourl'); ?></label></th>
                     <td><input size="36" type="text" name="lockets_feedlogourl" id="lockets_feedlogourl" value="<?php
-echo attribute_escape($lockets_feedlogourl); ?>" /><br>logoのURLをhttpから入れてください。</td>
+echo attribute_escape($lockets_feedlogourl); ?>" /><br>サイトロゴのURLをhttpから入れてください。</td>
                 </tr>
                 
                 <tr>
